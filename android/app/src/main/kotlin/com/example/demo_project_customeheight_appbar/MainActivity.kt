6a0ba45504b1eb7635643a1f2d927444366d4853
nando <muhammad.rezki.ananda@gmail.com>
2021-04-02 18:20:36 +0700

    INITIAL COMMIT

---
+++ android/app/src/main/kotlin/com/example/demo_project_customeheight_appbar/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.demo_project_customeheight_appbar
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: PreferredSize(
+            preferredSize: Size.fromHeight(200),
+            child: AppBar(
+              backgroundColor: Colors.amber,
+              flexibleSpace: Stack(children: <Widget>[
+                Positioned(
+                  top: 130,
+                  right: 0,
+                  child: Container(
+                    margin: EdgeInsets.all(20),
+                    child: Text(
+                      "Appbar with Custom Height",
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700),
+                    ),
+                  ),
+                ),
+              ]),
+            )),
+      ),
+    );
+  }
+}
